fba1db332cc200fac90ba98e1d5a1840133c4caf
lino <lino@lino-XPS-13-9370>
2025-05-22 17:14:58 +0200

    in progress

--- src/main/java/iconic/mytrade/gutenbergPrinter/PrinterCommands.java
+++ src/main/java/iconic/mytrade/gutenbergPrinter/PrinterCommands.java
@@ -2748,7 +2748,6 @@ public class PrinterCommands extends iconic.mytrade.gutenbergInterface.PrinterCo
 		
 		private void AutoVoidTrx()
 		{
-			SharedPrinterFields.setMyReply(false);
 		}
 		
 		public static int  getPrinterState() throws JposException
